f6c59d8d0185169adaa1a0e3c0a9c298c3a2e28c
pjayas <115007528+pjayas@users.noreply.github.com>
2024-10-25 18:39:21 +0900

    v.1

--- Assets/Script/Manager/UI_Manager.cs
+++ Assets/Script/Manager/UI_Manager.cs
@@ -45,7 +45,7 @@ public class UI_Manager : MonoBehaviour
     void Update()
     {
         realTime += Time.deltaTime;
-        HP -= Time.deltaTime + (realTime * 0.0001f);
+        HP -= Time.deltaTime + (realTime * 0.005f);
         image.fillAmount = (HP / HP_full);
 
         if (HP <= 0)
